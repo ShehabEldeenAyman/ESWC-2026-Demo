70cd3320486afe34419d4e04b2a04ed8637e43c3
ShehabEldeenAyman <shehab.ayman@vito.be>
2026-04-23 10:54:39 +0200

    .

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,8 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     allowedHosts: [
       'ed-pretelephone-unpleasantly.ngrok-free.dev'
     ]
